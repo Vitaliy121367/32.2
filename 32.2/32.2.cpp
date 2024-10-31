053852d5a134bb5c3b58c549c66efa5dd49a4dc0
Vitaliy <qw929496@gmail.com>
2024-10-31 18:51:35 +0200

    main

--- 32.2/32.2.cpp
+++ 32.2/32.2.cpp
@@ -10,13 +10,18 @@ using std::endl;
 
 int main()
 {
-    /*Products::Fun::Book book("451 degrees Fahrenheit","Ray Bradbury",170,"Educational book Bohdan","Dystopia",158,12);
-    book.show();
-    cout << "Calc Price: " << book.calcPrice();*/
+    Products::Fun::Book book("451 degrees Fahrenheit","Ray Bradbury",170,"Educational book Bohdan","Dystopia",158,12);
 
-    /*Products::Fun::Toy toy("Wooden puzzle Lion Mosaic", 279, "Wood Ukraine", "Wood", 3);
-    toy.show();
-    cout << "Calc Price: " << toy.calcPrice();*/
+    Products::Fun::Toy toy("Wooden puzzle Lion Mosaic", 279, "Wood Ukraine", "Wood", 3);
+
+    Lottery ItStep;
+    ItStep.addProdact(new Products::Fun::Book("451 degrees Fahrenheit", "Ray Bradbury", 170, "Educational book Bohdan", "Dystopia", 158, 12));
+    ItStep.addProdact(new Products::Fun::Toy("Wooden puzzle Lion Mosaic", 279, "Wood Ukraine", "Wood", 3));
+    ItStep.addProdact(new Products::Fun::Game("Monopoly",560,"Pusle",12,4));
+    ItStep.addProdact(new Products::Sport::Clothing("Jacket", 650, "Ukraine", 30,"Overcoat", 52));
+    ItStep.rndProdact();
+    cout << "-----------------------------------------------\n\n";
+    ItStep.showAll();
 
     return 0;
 }
